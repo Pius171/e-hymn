4bf87ea76d73ead6d1ef702701828316bc0fb24c
Pius <70412012+Pius171@users.noreply.github.com>
2022-08-30 23:29:57 +0100

    Add files via upload

--- ehymn2/jsoncreator.py
+++ ehymn2/jsoncreator.py
@@ -4,21 +4,25 @@ import json
 myDict={}
 values_list=[]
 
-with open('hymns.json') as d:
+with open('data/hymns.json') as d:
     myDict=json.load(d)
 
 values_list=list(myDict.values())
 #print(values_list)
 #print(max(values_list))
 while True:
-    hymn, number = input("Type in hymn and hymn number separated by a whitespace: ").split(" ")
+    hymn, number = input("Type in hymn and hymn number separated by a whitespace: ").split(":")
     #print(hymn,number)
     if hymn not in myDict.keys() and number not in myDict.values():
         myDict.update({hymn:int(number)})
-        print(myDict)
+        #print(myDict)
 
     else:
         print("already exist")
     
-    with open('hymns.json', 'w') as fp:
-        json.dump(myDict, fp)
+    with open('data/hymns.json', 'w') as fp:
+        json.dump(myDict, fp,indent=1)
+        print(json.dumps(myDict,indent=1))
+        
+        
+        
